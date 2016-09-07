72562af8d064be1abda38cf6780b2ff1ef973c61
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-09-07 15:25:56 +0900

    Improve code and fix double messaging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -8,6 +9,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/boltdb/bolt"
 	"github.com/nlopes/slack"
 )
 
@@ -21,15 +23,94 @@ var (
 	textKeywords         = []string{"ваканси", "работа", "позици", "тестировщик", "автоматизатор", "должность", "требования"}
 	linkKeywords         = []string{"hh.ru", "job", "linkedin", "position", "vacancy", "work", "career"}
 	fromID, toID, userID string
-	rtm                  *slack.RTM
-	api                  *slack.Client
 )
 
-const regex = "(http|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?"
+const (
+	BUCKET           = "QA-SLACK"
+	REGEX            = "(http|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?"
+	WRONG_CHANNEL_ID = "Wrong channel ID"
+	WRONG_USER_ID    = "Wrong user ID"
+	NOT_JOB_POSTING  = "Not job posting"
+	ALREADY_POSTED   = "Already posted"
+)
+
+type Slacker interface {
+	Repost(string, string) error
+	Delete(string, string) error
+}
+
+type SlackerClient struct {
+	Slack *slack.Client
+}
+
+func (c SlackerClient) Repost(toID, text string) error {
+	params := slack.PostMessageParameters{
+		AsUser: true,
+	}
+	_, _, err := c.Slack.PostMessage(toID, text, params)
+	return err
+}
+
+func (c SlackerClient) Delete(toID, timestamp string) error {
+	_, _, err := c.Slack.DeleteMessage(toID, timestamp)
+	return err
+}
+
+type SlackClient struct {
+	Client  Slacker
+	Storage *bolt.DB
+}
+
+func (c *SlackClient) RepostMessage(ev *slack.MessageEvent, r *regexp.Regexp) error {
+	if ev.Channel != fromID {
+		return errors.New(WRONG_CHANNEL_ID)
+	}
+	if len(ev.Attachments) > 0 {
+		return errors.New(NOT_JOB_POSTING)
+	}
+	text := ev.Text
+	if ev.SubMessage != nil && ev.SubMessage.Text != "" {
+		text = ev.SubMessage.Text
+	}
+	if !isJobPosting(text, r) {
+		return errors.New(NOT_JOB_POSTING)
+	}
+	text = strings.Replace(text, "<", "", -1)
+	text = strings.Replace(text, ">", "", -1)
+	if alreadyPosted(text, c.Storage) {
+		return errors.New(ALREADY_POSTED)
+	}
+	savePosted(text, c.Storage)
+	err := c.Client.Repost(toID, text)
+	return err
+}
+
+func (c *SlackClient) DeleteMessage(ev *slack.MessageEvent) error {
+	if ev.Channel != toID {
+		return errors.New(WRONG_CHANNEL_ID)
+	}
+	if ev.User == userID {
+		return errors.New(WRONG_USER_ID)
+	}
+	err := c.Client.Delete(toID, ev.Timestamp)
+	return err
+}
 
 func main() {
 	flag.Parse()
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	/**debug = true
+	*token = "xoxb-70959650517-a1ZNrKZeEwJum1PMQu0zlsdP"
+	*fromChannel = "jobs"
+	*toChannel = "vacancies"
+	*slackUser = "recruiters"*/
+
+	/**debug = true
+	*token = "xoxp-2693983659-5089661906-67807438769-3e87bc69f7"
+	*fromChannel = "jobs"
+	*toChannel = "vacancies"
+	*slackUser = "artemnikitin"*/
 
 	if *token == "" || *fromChannel == "" || *toChannel == "" || *slackUser == "" {
 		fmt.Println("Specify correct flags")
@@ -37,42 +118,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	r, err := regexp.Compile(regex)
+	db, err := bolt.Open("repost.db", 0600, nil)
+	if err != nil {
+		log.Fatal("Can't open DB: ", err)
+	}
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(BUCKET))
+		return err
+	})
+	if err != nil {
+		log.Fatal("Can't create bucket: ", err)
+	}
+
+	r, err := regexp.Compile(REGEX)
 	if err != nil {
 		log.Fatal("Can't compile regexp: ", err)
 	}
 
-	api = slack.New(*token)
+	api := slack.New(*token)
 	api.SetDebug(*debug)
-
-	users, err := api.GetUsers()
-	if err != nil {
-		log.Println("Can't get list of users:", err)
-		return
-	}
-	for _, user := range users {
-		if user.Name == *slackUser {
-			userID = user.ID
-			break
-		}
+	client := &SlackClient{
+		Client: SlackerClient{
+			Slack: api,
+		},
+		Storage: db,
 	}
 
-	channels, err := api.GetChannels(false)
-	if err != nil {
-		log.Println("Can't get list of channels:", err)
-		return
-	}
-	for _, channel := range channels {
-		switch channel.Name {
-		case *fromChannel:
-			fromID = channel.ID
-		case *toChannel:
-			toID = channel.ID
-		default:
-		}
-	}
+	getSlackUserID(api)
+	getSlackChannelID(api)
 
-	rtm = api.NewRTM()
+	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
 	for {
@@ -81,8 +157,8 @@ func main() {
 			fmt.Println("Event Received")
 			switch ev := msg.Data.(type) {
 			case *slack.MessageEvent:
-				processMessageIfNeeded(ev, r)
-				deleteMessageIfNeeded(ev)
+				client.RepostMessage(ev, r)
+				client.DeleteMessage(ev)
 
 			case *slack.RTMError:
 				fmt.Printf("Error: %s\n", ev.Error())
@@ -96,31 +172,6 @@ func main() {
 		}
 	}
 
-}
-
-func processMessageIfNeeded(ev *slack.MessageEvent, r *regexp.Regexp) {
-	if ev.Channel == fromID {
-		if ev.SubType == "message_changed" && len(ev.SubMessage.Attachments) > 0 {
-			return
-		}
-		text := ev.Text
-		if ev.SubMessage != nil && ev.SubMessage.Text != "" {
-			text = ev.SubMessage.Text
-		}
-		if isJobPosting(text, r) {
-			text = strings.Replace(text, "<", "", -1)
-			text = strings.Replace(text, ">", "", -1)
-			rtm.SendMessage(rtm.NewOutgoingMessage(text, toID))
-		}
-	}
-}
-
-func deleteMessageIfNeeded(ev *slack.MessageEvent) {
-	if ev.Channel == toID {
-		if ev.User != userID {
-			api.DeleteMessage(toID, ev.Timestamp)
-		}
-	}
 }
 
 func isJobPosting(text string, r *regexp.Regexp) bool {
@@ -142,4 +193,56 @@ func containsKeyword(text string, list []string) bool {
 
 func isNotSlackURL(text string) bool {
 	return !strings.Contains(text, ".slack.com")
+}
+
+func getSlackUserID(api *slack.Client) {
+	users, err := api.GetUsers()
+	if err != nil {
+		log.Fatal("Can't get list of users:", err)
+	}
+	for _, user := range users {
+		if user.Name == *slackUser {
+			userID = user.ID
+			break
+		}
+	}
+}
+
+func getSlackChannelID(api *slack.Client) {
+	channels, err := api.GetChannels(false)
+	if err != nil {
+		log.Fatal("Can't get list of channels:", err)
+	}
+	for _, channel := range channels {
+		switch channel.Name {
+		case *fromChannel:
+			fromID = channel.ID
+		case *toChannel:
+			toID = channel.ID
+		default:
+		}
+	}
+}
+
+func alreadyPosted(text string, db *bolt.DB) bool {
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BUCKET))
+		bytes := bucket.Get([]byte(text))
+		if bytes != nil {
+			return errors.New("")
+		}
+		return nil
+	})
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+func savePosted(text string, db *bolt.DB) {
+	db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BUCKET))
+		err := bucket.Put([]byte(text), []byte(text))
+		return err
+	})
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"math/rand"
 	"regexp"
 	"testing"
+
+	"github.com/boltdb/bolt"
+	"github.com/nlopes/slack"
 )
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// TestClient test implementation of Slacker interface
+type TestClient struct {
+}
+
+func (c TestClient) Repost(toID, text string) error {
+	return nil
+}
+
+func (c TestClient) Delete(toID, timestamp string) error {
+	return nil
+}
 
 func TestRegexp(t *testing.T) {
 	cases := []struct {
@@ -18,7 +36,7 @@ func TestRegexp(t *testing.T) {
 		{"dsssdsdsd http://example.com  dfdf f- dfd ", true},
 	}
 
-	r, _ := regexp.Compile(regex)
+	r, _ := regexp.Compile(REGEX)
 
 	for _, v := range cases {
 		result := r.MatchString(v.in)
@@ -54,10 +72,9 @@ func TestIsJobPosting(t *testing.T) {
 		{"[skype - tamara.mishcherina ]\nВсем привет! Открылась вакансия для QA automation (опыт от 3+) на удаленку. English level (speaking, writing, reading) - intermediate level. Автоматизация на С#. Пишите в личку, отвечу на все вопросы. :slightly_smiling_face:", false},
 		{"http://hh.ru/something", true},
 		{"something interesting http://example.com/jobs", true},
-		{"something interesting http://example.slack.com/jobs", false},
 	}
 
-	r, _ := regexp.Compile(regex)
+	r, _ := regexp.Compile(REGEX)
 
 	for _, v := range cases {
 		result := isJobPosting(v.in, r)
@@ -67,19 +84,142 @@ func TestIsJobPosting(t *testing.T) {
 	}
 }
 
-func TestIsNotSlackURL(t *testing.T) {
+func TestSlackClientRepost(t *testing.T) {
 	cases := []struct {
-		in  string
-		res bool
+		msg       *slack.MessageEvent
+		res, desc string
 	}{
-		{"fgfvfvfvfvfv", true},
-		{"https://softwaretesters.slack.com/", false},
+		{&slack.MessageEvent{
+			Msg: slack.Msg{
+				Channel: "222",
+			},
+		}, WRONG_CHANNEL_ID, "Wrong ID"},
+		{&slack.MessageEvent{
+			Msg: slack.Msg{
+				Channel: "111",
+				Text:    "wqeqewqewq",
+			},
+		}, NOT_JOB_POSTING, "Incorrect text"},
+		{&slack.MessageEvent{
+			Msg: slack.Msg{
+				Channel: "111",
+				Text:    "http://hh.ru",
+			},
+			SubMessage: &slack.Msg{
+				Text: "wqeqweqeew",
+			},
+		}, NOT_JOB_POSTING, "Initially correct text, but incorrect after transformation"},
+		{&slack.MessageEvent{
+			Msg: slack.Msg{
+				Channel:     "111",
+				Text:        "http://hh.ru",
+				Attachments: []slack.Attachment{{}},
+			},
+		}, NOT_JOB_POSTING, "Correct text with attachments"},
+	}
+
+	fromID = "111"
+	r, _ := regexp.Compile(REGEX)
+	client := &SlackClient{
+		Client: TestClient{},
 	}
 
 	for _, v := range cases {
-		result := isNotSlackURL(v.in)
-		if result != v.res {
-			t.Errorf("For string: %s, actual result: %v, expected: %v", v.in, result, v.res)
+		err := client.RepostMessage(v.msg, r)
+		if err == nil {
+			t.Errorf("For case: %s, error shouldn't be nil!", v.desc)
+		}
+		if err != nil && err.Error() != v.res {
+			t.Errorf("For case: %s, actual error:%s, expected: %s", v.desc, err.Error(), v.res)
 		}
 	}
+}
+
+func TestAlreadyPostedMessageShouldntBePostedTwice(t *testing.T) {
+	// Initialization of test DB
+	db, err := bolt.Open("test.db", 0600, nil)
+	if err != nil {
+		t.Fatal("Can't open DB: ", err)
+	}
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(BUCKET))
+		return err
+	})
+	if err != nil {
+		t.Fatal("Can't create bucket: ", err)
+	}
+
+	// Post double message
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BUCKET))
+		err := bucket.Put([]byte("test http://hh.ru"), []byte("test http://hh.ru"))
+		return err
+	})
+	if err != nil {
+		t.Fatal("Can't add entry: ", err)
+	}
+
+	// Prepare test data
+	fromID = "111"
+	r, _ := regexp.Compile(REGEX)
+	ev := &slack.MessageEvent{
+		Msg: slack.Msg{
+			Channel: "111",
+			Text:    "test http://hh.ru",
+		},
+	}
+	client := &SlackClient{
+		Client:  TestClient{},
+		Storage: db,
+	}
+
+	err = client.RepostMessage(ev, r)
+	if err == nil {
+		t.Error("Should return an error on attempt to repost already reposted message")
+	}
+}
+
+func TestNewMessageShouldBeReposted(t *testing.T) {
+	// Initialization of test DB
+	db, err := bolt.Open("test.db", 0600, nil)
+	if err != nil {
+		t.Fatal("Can't open DB: ", err)
+	}
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(BUCKET))
+		return err
+	})
+	if err != nil {
+		t.Fatal("Can't create bucket: ", err)
+	}
+
+	// Prepare test data
+	fromID = "111"
+	r, _ := regexp.Compile(REGEX)
+	ev := &slack.MessageEvent{
+		Msg: slack.Msg{
+			Channel: "111",
+			Text:    "test http://hh.ru " + randomString(50),
+		},
+	}
+	client := &SlackClient{
+		Client:  TestClient{},
+		Storage: db,
+	}
+
+	err = client.RepostMessage(ev, r)
+	if err != nil {
+		t.Error("Should return ok for repost of a new message")
+	}
+
+}
+
+func randomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
